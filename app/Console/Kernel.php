5c8a79f42e841cdf3618530c048724094f34deb8
iyoramu <yirutabyose@gmail.com>
2025-05-23 21:40:23 +0200

    feat(console): add Kernel class to manage console commands
    Introduce the Kernel class to handle the registration and execution of console commands. This includes commands for making controllers, models, middleware, and handling migrations and serving the application. The class also ensures the console executable is available post-autoload.

---
+++ app/Console/Kernel.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace BananaPHP\Console;
+
+use Symfony\Component\Console\Application;
+use BananaPHP\Console\Commands\MakeController;
+use BananaPHP\Console\Commands\MakeModel;
+use BananaPHP\Console\Commands\MakeMiddleware;
+use BananaPHP\Console\Commands\MigrateCommand;
+use BananaPHP\Console\Commands\ServeCommand;
+
+class Kernel
+{
+    private Application $application;
+
+    public function __construct()
+    {
+        $this->application = new Application('BANANA-PHP Console', '1.0.0');
+        
+        $this->registerCommands();
+    }
+
+    public static function postAutoloadDump(): void
+    {
+        if (!file_exists($console = __DIR__.'/../../bin/banana')) {
+            copy(__DIR__.'/../../stubs/console.stub', $console);
+            chmod($console, 0755);
+        }
+    }
+
+    private function registerCommands(): void
+    {
+        $this->application->add(new MakeController());
+        $this->application->add(new MakeModel());
+        $this->application->add(new MakeMiddleware());
+        $this->application->add(new MigrateCommand());
+        $this->application->add(new ServeCommand());
+    }
+
+    public function handle(): int
+    {
+        return $this->application->run();
+    }
+}
