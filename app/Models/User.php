4c20259c420d611648567255169f049cf0312a0d
iyoramu <yirutabyose@gmail.com>
2025-05-24 00:19:08 +0200

    refactor(User): simplify User model by removing unused methods and attributes
    The User model was simplified by removing the `register` and `verifyCredentials` methods, as well as the `fillable` and `hidden` attributes. These changes were made to streamline the model and reduce unnecessary complexity, as the functionality is now handled by other parts of the application.

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,38 +1,14 @@
 <?php
 declare(strict_types=1);
 
-namespace BananaPHP\Models;
+namespace App\Models;
 
-use BananaPHP\Services\Auth\PasswordService;
+use BananaPHP\Models\BaseModel;
 
 class User extends BaseModel
 {
     protected string $table = 'users';
     protected string $primaryKey = 'id';
-    protected array $fillable = [
-        'name', 'email', 'password'
-    ];
-    protected array $hidden = [
-        'password'
-    ];
-
-    public static function register(array $data): int
-    {
-        $data['password'] = PasswordService::hash($data['password']);
-        return parent::create($data);
-    }
-
-    public static function verifyCredentials(string $email, string $password): ?array
-    {
-        $user = static::query()
-            ->where('email', '=', $email)
-            ->first();
-
-        if ($user && PasswordService::verify($password, $user['password'])) {
-            unset($user['password']);
-            return $user;
-        }
-
-        return null;
-    }
+    protected array $fillable = [];
+    protected array $hidden = [];
 }
