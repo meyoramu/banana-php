e9d4b81a9b276b795cc2822e658aa4b48ebc5781
iyoramu <yirutabyose@gmail.com>
2025-05-24 00:03:57 +0200

    refactor(BaseModel): simplify query handling by removing unused methods
    The `create`, `update`, and `delete` methods were removed as they were not being used. The `query` method was refactored to use a new `newQuery` method, which simplifies the instantiation of `QueryBuilder`. This improves maintainability and reduces unnecessary code.

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 namespace BananaPHP\Models;
 
 use BananaPHP\Services\Database\QueryBuilder;
-use BananaPHP\Services\Database\Connection;
 
 abstract class BaseModel
 {
@@ -12,18 +11,16 @@ abstract class BaseModel
     protected string $primaryKey = 'id';
     protected array $fillable = [];
     protected array $hidden = [];
-    protected QueryBuilder $queryBuilder;
-
-    public function __construct()
-    {
-        $this->queryBuilder = new QueryBuilder(Connection::getInstance());
-        $this->queryBuilder->table($this->table);
-    }
 
     public static function query(): QueryBuilder
     {
-        $model = new static();
-        return $model->queryBuilder;
+        return (new static())->newQuery();
+    }
+
+    protected function newQuery(): QueryBuilder
+    {
+        $query = new QueryBuilder(Connection::getInstance());
+        return $query->table($this->table);
     }
 
     public static function all(): array
@@ -34,29 +31,5 @@ abstract class BaseModel
     public static function find(int $id): ?array
     {
         return static::query()->where((new static())->primaryKey, '=', $id)->first();
-    }
-
-    public static function create(array $data): int
-    {
-        $model = new static();
-        $filteredData = array_intersect_key($data, array_flip($model->fillable));
-        return static::query()->insert($filteredData);
-    }
-
-    public static function update(int $id, array $data): int
-    {
-        $model = new static();
-        $filteredData = array_intersect_key($data, array_flip($model->fillable));
-        return static::query()
-            ->where($model->primaryKey, '=', $id)
-            ->update($filteredData);
-    }
-
-    public static function delete(int $id): int
-    {
-        $model = new static();
-        return static::query()
-            ->where($model->primaryKey, '=', $id)
-            ->delete();
     }
 }
