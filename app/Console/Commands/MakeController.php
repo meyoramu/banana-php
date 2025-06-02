9ad5d50ef99f734b96fdb6b9dc397c7ed10e5778
IRUTABYOSE Yoramu <iyoramu@gmail.com>
2025-06-02 22:46:31 +0200

    refactor: update namespace in MakeController command
    The namespace was changed from BananaPHP\Console\Commands to App\Console\Commands to align with the current application structure.

--- app/Console/Commands/MakeController.php
+++ app/Console/Commands/MakeController.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace BananaPHP\Console\Commands;
+namespace App\Console\Commands;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
